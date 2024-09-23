0277a177aee498568b99d4ca9b2427ac81eb7658
Breno Santos <brenosantos145@gmail.com>
2024-09-23 18:00:25 -0300

    feat: creating checkbox to material, slider for max clicks and reset button

--- sketch.js
+++ sketch.js
@@ -1,17 +1,46 @@
 let angle = 0;
 let sponge = [];
+let MAX_CLICKS = 2;
 let clicks = 0;
+
+let checkbox;
+let range;
+let resetBtn;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-  normalMaterial();
 
   const b = new Box(0, 0, 0, 200);
   sponge.push(b);
+
+  checkbox = createCheckbox(' normal material', false);
+  checkbox.changed(() => {
+    if (checkbox.checked()) {
+      normalMaterial();
+    } else {
+      emissiveMaterial(0, 0, 0);
+    }
+  });
+
+  range = createSlider(1, 3, 2, 1);
+  range.changed(() => {
+    MAX_CLICKS = range.value();
+  });
+
+  resetBtn = createButton('reset');
+  resetBtn.mousePressed(() => {
+    sponge = [];
+    clicks = 0;
+
+    const b = new Box(0, 0, 0, 200);
+    sponge.push(b);
+  });
 }
 
-function mousePressed() {
-  if (clicks >= 2) return;
+function mousePressed({ target }) {
+  if (target.id !== 'defaultCanvas0') return;
+
+  if (clicks >= MAX_CLICKS) return;
   let newSponge = [];
   for (let b of sponge) {
     let newBoxes = b.generate();
